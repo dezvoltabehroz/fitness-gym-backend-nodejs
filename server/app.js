a5a8e009417a8db3f45d6a654ec47330dec99152
dezvoltabehroz <behroz.ahmed@dezvoltatech.net>
2021-03-25 00:47:35 +0500

    UI has been added
    UI has been added

--- server/app.js
+++ server/app.js
@@ -14,17 +14,15 @@ dotenv.config();
 // Body Parser Configuration
 app.use(express.json({ limit: "250mb" }));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Get our API routes
 const index_routes = require("./routes");
 app.use('/api/registration', index_routes.reg_api);
 app.use('/api/profile', index_routes.profile_api);
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
