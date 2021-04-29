2aed712849f5de4d13b790749ad4dd7e9bd81b92
dezvoltabehroz <behroz.ahmed@dezvoltatech.net>
2021-04-29 20:30:16 +0500

    Update BookingHelper.js

--- server/helper/BookingHelper.js
+++ server/helper/BookingHelper.js
@@ -100,7 +100,12 @@ exports.getBookings = (req, res) => {
                                     if (result.length == (index + 1)) {
                                         filteringArrays(newArraySlots)
                                             .then(filterArray => {
-                                                common.resOnSuccess(res, true, "Booking List has been fetched successfully", filterArray)
+                                                let data = {
+                                                    start_time: data_schedule.start_time,
+                                                    end_time: data_schedule.end_time,
+                                                    filterArray: filterArray
+                                                }
+                                                common.resOnSuccess(res, true, "Booking List has been fetched successfully", data)
                                             })
                                     }
                                 }
