b052d05c7364cc4ae15b02af89c7b697294556d4
dezvoltabehroz <behroz.ahmed@dezvoltatech.net>
2021-05-12 14:43:14 +0500

    Update ProfileHelper.js

--- server/helper/ProfileHelper.js
+++ server/helper/ProfileHelper.js
@@ -164,7 +164,7 @@ exports.listAllBookings = (req, res) => {
         .then(bookingData => {
             if (bookingData.length > 0) {
                 let list_booking = [];
-                bookingData.forEach(async (dataBooking, index) => {
+                bookingData.map(async (dataBooking, index) => {
                     let booking_slots_query = `SELECT COUNT(*) AS booked_slots FROM booking WHERE booking_start_time = '${dataBooking.booking_start_time}' AND booking_end_time = '${dataBooking.booking_end_time}' AND booking_date = '${moment(dataBooking.booking_date).format('yyyy-MM-DD')}' and is_cancel = 0`
                     await query.executeQuery(booking_slots_query)
                         .then(slotsData => {
