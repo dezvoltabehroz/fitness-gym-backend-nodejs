5af105f8cd252096e905666cf86a75b79a68b6c2
dezvoltabehroz <behroz.ahmed@dezvoltatech.net>
2021-05-15 19:05:00 +0500

    Update BookingHelper.js

--- server/helper/BookingHelper.js
+++ server/helper/BookingHelper.js
@@ -210,7 +210,7 @@ function bookingSlotsArray(id, date, start_time, end_time) {
                     bookingSlots.is_booked = slotsData[0].is_booked
                     bookingSlots.is_blocked = slotsData[0].is_blocked
                     bookingSlots.is_unavailable = slotsData[0].is_unavailable
-                }else{
+                } else {
                     bookingSlots.booked_slots = 0
                     bookingSlots.is_booked = 0
                     bookingSlots.is_blocked = 0
@@ -241,10 +241,17 @@ function bookingSlotsArray(id, date, start_time, end_time) {
 
                     await query.executeQuery(booking_slots_query)
                         .then(slotsData2 => {
-                            bookingSlots.booked_slots = slotsData2[0].booked_slots
-                            bookingSlots.is_booked = slotsData2[0].is_booked
-                            bookingSlots.is_blocked = slotsData2[0].is_blocked
-                            bookingSlots.is_unavailable = slotsData2[0].is_unavailable
+                            if (slotsData2.length > 0) {
+                                bookingSlots.booked_slots = slotsData2[0].booked_slots
+                                bookingSlots.is_booked = slotsData2[0].is_booked
+                                bookingSlots.is_blocked = slotsData2[0].is_blocked
+                                bookingSlots.is_unavailable = slotsData2[0].is_unavailable
+                            }else{
+                                bookingSlots.booked_slots = 0
+                                bookingSlots.is_booked = 0
+                                bookingSlots.is_blocked = 0
+                                bookingSlots.is_unavailable = 0
+                            }
                             timeSlots.push(bookingSlots);
 
                             if (bookingSlots.booking_end_time == end_time)
