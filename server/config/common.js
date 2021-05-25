0332999631a73a9f9d9adc924988624af4d52b29
dezvoltabehroz <behroz.ahmed@dezvoltatech.net>
2021-05-25 23:41:49 +0500

    Update common.js

--- server/config/common.js
+++ server/config/common.js
@@ -114,12 +114,23 @@ module.exports = {
             subject: `Educo Gym Password`,
             html: `
             <p>
-                <b><i>Your Password for Application : ${password}</i></b>
+                <img src="https://educogym.s3-eu-west-1.amazonaws.com/email_images/email_image.png" />
             </p>
             <p>
-                Thank you!<br/>
-                Team Educo Gym<br/>
-                <a href="http://educogymecclesstreet.com/">Educo GYm</a>
+                Welcome to Educogym Eccles Street! <br/>
+                These are your login details: --------- <br/>
+                <b><i>Your Password for Application : ${password}</i></b><br/>
+                Use the app to book all of your workouts from now on and contact reception for assessment bookings.
+            </p>
+            <p>
+                You have made a great decision! We are based on research that shows the mind and body are deeply connected and should be trained together for optimum results.<br/>
+                The research was conducted by Dr. Tony Quinn and involved a full body transformation where participants lost 7.5lbs fat and gained 3lbs muscle in just 12 days. <br/>
+                It was shown the key to these extraordinary results was learning to use a greater part of the mind that allowed the person to draw out their potential /perfect shape from within. <br/>
+                We recommend using the two mp3 files Start a new life today and Relaxation with a purpose from Educohealth  in conjunction with the nutrition and training we will provide to achieve your very best results with us. <br/>
+            </p>
+            <p>
+                Yours in health, <br/>
+                Educogym Eccles Street<br/>
             </p>
             `
         };
